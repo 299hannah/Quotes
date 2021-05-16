cd210f144b0c879cdc4ef27ed8a60b14d9f6be9a
hannah <hannahmwangiwambui@gmail.com>
2021-05-17 01:41:45 +0300

    Added object in class

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,6 +4,7 @@ export class Quote {
     constructor(public id: number, public author: string, public name: string,
       public description: string, public publishedDate: Date ,public likes: number  ,public dislikes: number){
        this.showDescription=false;
+       
     }
   }
   
