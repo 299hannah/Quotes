4c6e08ff692f628e23b1556f4bd285c9d60bfb88
hannah <hannahmwangiwambui@gmail.com>
2021-05-16 23:01:27 +0300

    quote changes

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,8 +1,9 @@
 export class Quote {
   
-  public showDescription: boolean;
-  dislikes: number;
-    constructor(public id: number, public author: string, public name: string,public description: string, public publishedDate: Date){
+  public showDescription: boolean; 
+    constructor(public id: number, public author: string, public name: string,
+      public description: string, public publishedDate: Date ,public likes: number  ,public dislikes: number){
        this.showDescription=false;
     }
   }
+  
